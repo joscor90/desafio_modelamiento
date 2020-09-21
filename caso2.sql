f5fa7242a81bf5b035ccd50b8bdc4ee2b9855710
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-21 16:51:43 -0300

    Modified caso2.sql file based on the physical data model

---
+++ caso2.sql
@@ -0,0 +1,24 @@
+-- Creación de la base de datos caso2
+
+CREATE DATABASE caso2;
+
+-- Creación de la tabla cursos
+
+CREATE TABLE cursos(id SERIAL, PRIMARY KEY (id));
+
+-- Creación de la tabla profesores
+
+CREATE TABLE profesores(id SERIAL, nombre VARCHAR(50) NOT NULL, departamento VARCHAR(100) NOT NULL, PRIMARY KEY (id));
+
+
+-- Creación de la tabla alumnos 
+
+CREATE TABLE alumnos(id SERIAL, nombre VARCHAR(50) NOT NULL, rut INT NOT NULL, curso INT NOT NULL, PRIMARY KEY (id), FOREIGN KEY (curso) REFERENCES cursos(id));
+
+-- Creación de la tabla pruebas
+
+CREATE TABLE pruebas(id SERIAL, puntaje INT NOT NULL, profesor INT NOT NULL, PRIMARY KEY (id), FOREIGN KEY (profesor) REFERENCES profesores(id));
+
+-- Creación de la tabla pruebas_rendidas
+
+CREATE TABLE pruebas_rendidas(id_alumno INT, id_prueba INT, FOREIGN KEY (id_alumno) REFERENCES alumnos(id), FOREIGN KEY (id_prueba) REFERENCES pruebas(id));
